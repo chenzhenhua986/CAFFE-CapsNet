9a885ae7abe7e2ba7cc97102bc6e013ce7fd3cc4
chenzhenhua986 <chenzhenhua986@gmail.com>
2018-09-12 02:17:08 -0400

    Update squash_layer.hpp
    Remove gpu inerface

--- include/caffe/layers/squash_layer.hpp
+++ include/caffe/layers/squash_layer.hpp
@@ -23,10 +23,10 @@ class SquashLayer : public NeuronLayer<Dtype> {
 
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
+  //virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top);
 
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  //virtual void Backward_gpu(const vector<Blob<Dtype>*>& top, const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
   // intemediate variable
   Blob<Dtype> v_;
   int capsule_dim_;
