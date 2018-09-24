eb0e2be64f3421794cf8298d3d63618b7f41948c
chenzhenhua986 <chenzhenhua986@gmail.com>
2018-09-24 18:45:24 -0400

    Update squash_layer.cpp

--- src/caffe/layers/squash_layer.cpp
+++ src/caffe/layers/squash_layer.cpp
@@ -23,22 +23,10 @@ void SquashLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
 }
 
 template<typename Dtype>
-void squash(const Dtype* s, const int len, Dtype* v, Dtype* sum){
-  caffe_copy(len, s, v);
-  caffe_powx(len, v, Dtype(2), sum);
-  Dtype squared_norm = caffe_cpu_asum(len, sum) + 1e-07;
-  //LOG(INFO) << "norm: "<<squared_norm;
-  Dtype coefficient = (squared_norm / (squared_norm + 1)) / sqrt(squared_norm);
-  //LOG(INFO) << "coefficient: "<<coefficient;
-  caffe_scal(len, coefficient, v);
-}
-
-
-template<typename Dtype>
 Dtype cap_len(const Dtype* s, const int len, Dtype* v){
   caffe_powx(len, s, Dtype(2), v);
   Dtype squared_norm = caffe_cpu_asum(len, v) + 1e-07;
-  return sqrt(squared_norm);
+  return squared_norm;
 }
 
 template <typename Dtype>
@@ -47,11 +35,15 @@ void SquashLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   const Dtype* bottom_data = bottom[0]->cpu_data();
   Dtype* top_data = top[0]->mutable_cpu_data();
   Dtype* v_data = v_.mutable_cpu_data();
+  Dtype len;
   for (int i = 0; i < M_; ++i) {
     for (int j = 0; j < capsule_num_; ++j) {
-      squash(bottom_data + i * capsule_num_ * capsule_dim_ + j * capsule_dim_, capsule_dim_, 
-	top_data + i * capsule_num_*capsule_dim_ + j * capsule_dim_, v_data + 
-		i * capsule_num_ * capsule_dim_ + j * capsule_dim_);
+        len = cap_len(bottom_data + i * capsule_num_ * capsule_dim_ + j * capsule_dim_, capsule_dim_, 
+		     v_data + i * capsule_num_ * capsule_dim_ + j * capsule_dim_);
+	Dtype tmp = (1.0 - exp(-sqrt(len + 1e-7))) / sqrt(len + 1e-7);
+        //if(i == 0 && capsule_num_ == 10)
+          //LOG(INFO) << "cap len : "<<j<<", j: "<<len;
+	caffe_cpu_scale(capsule_dim_, tmp, bottom_data + i * capsule_num_ * capsule_dim_ + j * capsule_dim_, top_data + i * capsule_num_ * capsule_dim_ + j * capsule_dim_);
     }
   }
 }
@@ -64,15 +56,21 @@ void SquashLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const Dtype* bottom_data = bottom[0]->cpu_data();
     const Dtype* top_diff = top[0]->cpu_diff();
     Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
+    Dtype* v_data = v_.mutable_cpu_data();
     Dtype len;
     for (int i = 0; i < M_; ++i) {
       for (int j = 0; j < capsule_num_; ++j) {
         len = cap_len(bottom_data + i * capsule_num_ * capsule_dim_ + j * capsule_dim_, capsule_dim_, 
+		     v_data + i * capsule_num_ * capsule_dim_ + j * capsule_dim_);
+
+	// new squash function from paper: Capsule Network Performance on Complex Data
+	Dtype tmp = exp(-sqrt(len + 1e-7)) / (len + 1e-7) - 1.0 / sqrt(len + 1e-7) / (len + 1e-7) + exp(-sqrt(len + 1e-7)) / sqrt(len + 1e-7) / (len + 1e-7);
+	Dtype tmp1 = 1.0 / sqrt(len + 1e-7) - 1.0 / sqrt(len + 1e-7) * exp(-sqrt(len + 1e-7));
+	caffe_cpu_scale(capsule_dim_, tmp, v_data + i * capsule_num_ * capsule_dim_ + j * capsule_dim_, bottom_diff + i * capsule_num_ * capsule_dim_ + j * capsule_dim_);
+	caffe_add_scalar(capsule_dim_, tmp1, bottom_diff + i * capsule_num_ * capsule_dim_ + j * capsule_dim_);	
+	caffe_mul(capsule_dim_, top_diff + i * capsule_num_ * capsule_dim_ + j * capsule_dim_, 
+		bottom_diff + i * capsule_num_ * capsule_dim_ + j * capsule_dim_, 
 		bottom_diff + i * capsule_num_ * capsule_dim_ + j * capsule_dim_);
-	Dtype tmp = len/(1.0+pow(len, 2.0));
-        for (int k = 0; k < capsule_dim_; ++k) {
-          bottom_diff[i * capsule_num_ * capsule_dim_ + j * capsule_dim_ + k] = (1.0 / tmp - 2.0 * len) / pow((1.0+pow(len, 2.0)), 2.0) * top_diff[i * capsule_num_ * capsule_dim_ + j * capsule_dim_ + k] + tmp;
-        }
       }
     }
   }
